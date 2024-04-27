d0be0bac2b86b9ad0315c08422de79b8c0a94610
Elena Gonzalez <elenaglezrabal@gmail.com>
2024-04-27 09:50:12 +0100

    Leaving the game up to the button to generate random numbers

--- simon.js
+++ simon.js
@@ -1,12 +1,27 @@
 // globals
 let buttons = [];
+let game = [];
+
+/**
+ * Function to generate a random number between 0-3
+ */
+function getRandomInt() {
+  return Math.floor(Math.random() * 4) + 1;
+}
+
+/**
+ * Set up next turn
+ */
+function setUpNextTurn() {
+  game.push(getRandomInt());
+  document.querySelector(".debug #current-game").innerText = game.join("-");
+}
 
 /**
  * Generar estado inicial del juego
  */
 function initGame() {
   let button; // temporary variable for the newly created button
-
   let main = document.querySelector("main");
 
   for (let i of [1, 2, 3, 4]) {
@@ -14,6 +29,9 @@ function initGame() {
     button.innerText = buttons.length + 1;
     main.appendChild(button);
     buttons.push(button);
+    document
+      .querySelector("#next-turn")
+      .addEventListener("click", setUpNextTurn);
   }
 }
 
