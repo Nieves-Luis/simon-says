8aed372606607a595868a233d2c2210a2b6139e2
Aitor Val <yako83@gmail.com>
2024-04-27 12:37:19 +0200

    Added function play and play2

--- simon.js
+++ simon.js
@@ -29,10 +29,48 @@ function initGame() {
     button.innerText = buttons.length + 1;
     main.appendChild(button);
     buttons.push(button);
-    document
-      .querySelector("#next-turn")
-      .addEventListener("click", setUpNextTurn);
   }
+  document.querySelector("#next-turn").addEventListener("click", setUpNextTurn);
+  document.querySelector("#play").addEventListener("click", play2);
+}
+
+/**
+ * Add the class on to the buttons at the right time
+ */
+function play(event) {
+  let delay = 0;
+
+  for (let btnNumber of game) {
+    let button = buttons[btnNumber - 1];
+    function onOff() {
+      button.classList.toggle("on");
+    }
+    setTimeout(onOff, delay);
+
+    setTimeout(onOff, 500 + delay);
+    delay += 1000;
+  }
+  if (event) {
+    setTimeout(() => (event.target.disabled = false), delay);
+    event.target.disabled = true;
+  }
+}
+
+function play2() {
+  let remainingToSwitchOn = [...game];
+  function switchNext() {
+    if (remainingToSwitchOn.length) {
+      let btnNumber = remainingToSwitchOn.shift();
+      let button = buttons[btnNumber - 1];
+      button.classList.add("on");
+      setTimeout(() => {
+        button.classList.remove("on");
+      }, 500);
+    } else {
+      clearInterval(timer);
+    }
+  }
+  let timer = setInterval(switchNext, 1000);
 }
 
 initGame();
